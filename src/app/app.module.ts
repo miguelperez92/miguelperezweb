c3dd951c796de5312ca94da6417a886b00ff9070
Miguel Perez <info.miguelperez@gmail.com>
2019-07-19 18:18:29 +0200

    Add de todas las secciones de la pagina e incrustadas en el app component correctamente. Falta corregir algunas rutas a imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,27 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavigationComponent } from './components/shared/navigation/navigation.component';
 import { HeaderComponent } from './components/shared/header/header.component';
+import { IntroPageComponent } from './components/sections/intro/intro.component';
+import { ServicesPageComponent } from './components/sections/services/services.component';
+import { SkillsComponent } from './components/sections/skills/skills.component';
+import { FormacionComponent } from './components/sections/formacion/formacion.component';
+import { ExperienciaComponent } from './components/sections/experiencia/experiencia.component';
+import { ClientesComponent } from './components/sections/clientes/clientes.component';
+import { ContactoComponent } from './components/sections/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavigationComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroPageComponent,
+    ServicesPageComponent,
+    SkillsComponent,
+    FormacionComponent,
+    ExperienciaComponent,
+    ClientesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
