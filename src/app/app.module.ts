61d49f55730517d38c67bbd41516fd5edb518f00
Miguel Perez <info.miguelperez@gmail.com>
2019-07-19 18:00:07 +0200

    Se quita el navbar y en su lugar se crea un navigation y header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    NavigationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
